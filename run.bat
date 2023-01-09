f4203204417de988493f9b3e6009b85cfd3f1612
i.myasnikov <i.myasnikov@bars.ru>
2023-01-09 22:15:18 +0700

    init

--- run.bat
+++ run.bat
@@ -1,10 +1,11 @@
 set DOCKER_NAME=oracle_sql_11
 set DOCKER_IMAGE="oracleinanutshell/oracle-xe-11g"
 set DOCKER_HOSTNAME=ORACLE-SQL-11
-set ROOT_PATH_DB="%~dp0DB"
+set ROOT_PATH_DB="%~dp0oracle"
 set PORT_DB=49161
 
 rem docker run -d --rm -p %PORT_DB%:1521  -v %ROOT_PATH_DB%:/opt/oracle/oradata --name %DOCKER_NAME% --hostname %DOCKER_HOSTNAME%   %DOCKER_IMAGE%
-docker run -d --rm -p %PORT_DB%:1521 --name %DOCKER_NAME% --hostname %DOCKER_HOSTNAME%   %DOCKER_IMAGE%
+rem docker run -d --rm -p %PORT_DB%:1521 --name %DOCKER_NAME% --hostname %DOCKER_HOSTNAME%   -e ORACLE_ALLOW_REMOTE=true  -e ORACLE_ENABLE_XDB=true -v %ROOT_PATH_DB%:/u01/app/oracle %DOCKER_IMAGE%
+docker run -d --rm -p %PORT_DB%:1521 --name %DOCKER_NAME% --hostname %DOCKER_HOSTNAME%   -e ORACLE_ALLOW_REMOTE=true  -e ORACLE_ENABLE_XDB=true -v %ROOT_PATH_DB%:/u01/app/oracle %DOCKER_IMAGE%
 
 timeout 10
